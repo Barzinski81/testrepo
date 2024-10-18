18a7c5b506346e683e2bf5d20c8f83e1a8bbc387
Ivan Barzinski <i.barzinski@gmail.com>
2024-10-18 20:49:19 +0300

    Modified archiver.sh

--- archiver.sh
+++ archiver.sh
@@ -12,19 +12,16 @@ fi
 
 #tar -zcf $2 $1
 
-dir=$1
-file=$2
-
-if [ -d "$dir" ]; then
+if [ -d "$1" ]; then
   :
   else
-  echo "The directory '$dir' does not exist."
+  echo "The directory '$1' does not exist."
   echo "Please check if the provided path is correct."
   exit 1;
 fi
 
-if [ -f "$file" ]; then
-  echo "File with name '$file' already exists."
+if [ -f "$2" ]; then
+  echo "File with name '$2' already exists."
   echo "Please specify different file name or path."
   exit 1;
 fi
